2f65e5fc6df99c60d68546956d30e0fde6b5c118
GraemeB <grae22@gmail.com>
2017-03-01 13:29:55 +0200

    Connection string test.

--- PeepStat/Default.aspx.cs
+++ PeepStat/Default.aspx.cs
@@ -7,8 +7,7 @@ public partial class _Default : System.Web.UI.Page
 {
   //---------------------------------------------------------------------------
 
-  string DB_CONNECTION_STRING =
-    "Server=tcp:peepstat.database.windows.net,1433;Initial Catalog=PeepStat;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+  string DB_CONNECTION_STRING = Environment.GetEnvironmentVariable( "SQLAZURECONNSTR_defaultConnection" );
 
   //---------------------------------------------------------------------------
 
