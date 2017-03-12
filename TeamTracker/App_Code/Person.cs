d777bef2ca9f4bd54a1397ea1654e5f5ab9cfd73
j-broo <jordan.margaritov@gmail.com>
2017-03-12 18:04:25 +0200

    Update code references: extension -> contact

--- TeamTracker/App_Code/Person.cs
+++ TeamTracker/App_Code/Person.cs
@@ -6,7 +6,7 @@ public class Person
 
   public int Id { get; set; }
   public string Name { get; set; }
-  public string Extension { get; set; }
+  public string Contact { get; set; }
   public List<Status> Status { get; set; }
 
   //---------------------------------------------------------------------------
