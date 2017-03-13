5df9214a99f71deae034557a5f6e26e0c0b681d2
GraemeB <grae22@gmail.com>
2017-03-13 22:22:13 +0200

    Connection string.

--- TeamTracker/App_Code/Database.cs
+++ TeamTracker/App_Code/Database.cs
@@ -1,21 +1,23 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data.SqlClient;
 
 public class Database
 {
   //---------------------------------------------------------------------------
 
-  public const string DB_SERVER_NAME = "localhost";
+  public const string DB_SERVER_NAME = @"graemepc\sqlexpress";
   public const string DB_NAME = "TeamTracker";
   public const string DB_USERNAME = "TeamTrackerUser";
   public const string DB_PASSWORD = "TeamTrackerUser";
 
   public static readonly string DB_CONNECTION_STRING =
-    string.Format(
-      "Server={0};Database={1};User Id={2};Password={3};",
-      DB_SERVER_NAME,
-      DB_NAME,
-      DB_USERNAME,
-      DB_PASSWORD );
+    Environment.GetEnvironmentVariable( "SQLAZURECONNSTR_defaultConnection" ); 
+    //string.Format(
+    //  "Server={0};Database={1};User Id={2};Password={3};",
+    //  DB_SERVER_NAME,
+    //  DB_NAME,
+    //  DB_USERNAME,
+    //  DB_PASSWORD );
 
   //---------------------------------------------------------------------------
 
