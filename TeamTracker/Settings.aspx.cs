a08cc64e44ad40a9fff00ccc19b79a74b9fd23ff
GraemeB <grae22@gmail.com>
2017-03-24 22:16:18 +0200

    Password *'d out.

--- TeamTracker/Settings.aspx.cs
+++ TeamTracker/Settings.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web.UI.WebControls;
 using TeamTracker;
 
 public partial class Settings : System.Web.UI.Page
@@ -15,6 +16,26 @@ public partial class Settings : System.Web.UI.Page
     }
 
     dataSource.ConnectionString = Database.DB_CONNECTION_STRING;
+
+    settingsView.PreRender += Page_LoadComplete;
+  }
+
+  //---------------------------------------------------------------------------
+
+  protected void Page_LoadComplete( object sender, EventArgs e )
+  {
+    foreach( GridViewRow row in settingsView.Rows )
+    {
+      // Key contains 'password'?
+      if( row.Cells[ 1 ].Text.ToLower().Contains( "password" ) )
+      {
+        // If the 'value' cell doesn't have any controls (i.e. a textbox during edit mode).
+        if( row.Cells[ 2 ].Controls.Count == 0 )
+        {
+          row.Cells[ 2 ].Text = "*****";
+        }
+      }
+    }
   }
 
   //---------------------------------------------------------------------------
