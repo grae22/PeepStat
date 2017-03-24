6589fa5baad8141f8a5a8ca3e769dcd0d9ad69af
GraemeB <grae22@gmail.com>
2017-03-24 11:37:56 +0200

    Added unique constraints to StatusTypes name & sort order fields.

--- CreateDB.sql
+++ CreateDB.sql
@@ -195,6 +195,18 @@ CREATE TABLE [dbo].[StatusTypes](
 ) ON [PRIMARY]
 GO
 
+CREATE UNIQUE NONCLUSTERED INDEX [IX_StatusTypes_UniqueName] ON [dbo].[StatusTypes]
+(
+	[name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_StatusTypes_UniqueSortOrder] ON [dbo].[StatusTypes]
+(
+	[sortOrder] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
 
 -- Create export people view
 -----------------------------------------------------------------------------------------------------
